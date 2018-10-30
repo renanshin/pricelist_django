9c0aa7e477cb8735df954771db5a7ccf1bd93cfe
renanshin <43973984+renanshin@users.noreply.github.com>
2018-10-30 11:37:45 -0300

    Commit drag and drop

---
+++ projeto/produtos/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Produto
+
+def home(request):
+	produtos = Produto.objects.all()
+	return render(request, 'produtos.html',{"produtos":produtos})
+
+# Create your views here.
